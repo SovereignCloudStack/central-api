a3a84b9f312fed0d26497974f3f0927ff5aa89a5
Joshua Mühlfort <muehlfort@gonicus.de>
2024-07-24 13:23:20 +0000

    Add kubernetes xpkgs
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- crossplane-config/openstack-gen.py
+++ crossplane-config/openstack-gen.py
@@ -70,8 +70,8 @@ for crd_file in os.listdir("provider-openstack/package/crds"):
     local_xrd["metadata"]["name"] = f"x{crd['spec']['names']['plural']}.api.scs.community"
     local_xrd["spec"]["names"] = {"kind": f"x{crd['spec']['names']['kind'].lower()}", "plural": f"x{crd['spec']['names']['plural']}"}
     local_xrd["spec"]["claimNames"] = crd["spec"]["names"]
-    os.makedirs(f"apis/{crd['spec']['names']['singular']}", exist_ok=True)
-    with open(f"apis/{crd['spec']['names']['singular']}/definition.yaml", "w") as f:
+    os.makedirs(f"openstack/apis/{crd['spec']['names']['singular']}", exist_ok=True)
+    with open(f"openstack/apis/{crd['spec']['names']['singular']}/definition.yaml", "w") as f:
         f.write(yaml.dump(local_xrd))
 
     local_comp = copy.deepcopy(comp)
@@ -81,5 +81,5 @@ for crd_file in os.listdir("provider-openstack/package/crds"):
     local_comp["spec"]["resources"][0]["base"]["kind"] = crd['spec']['names']['kind']
     local_comp["spec"]["resources"][0]["base"]["apiVersion"] = crd['spec']['group'] + "/v1alpha1"
     local_comp["spec"]["resources"][0]["name"] = crd['spec']['names']['kind']
-    with open(f"apis/{crd['spec']['names']['singular']}/composition.yaml", "w") as f:
+    with open(f"openstack/apis/{crd['spec']['names']['singular']}/composition.yaml", "w") as f:
         f.write(yaml.dump(local_comp))
